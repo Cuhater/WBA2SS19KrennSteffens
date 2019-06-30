8b86ab43c384645a700c1db8a4f7b0a0f0372fb8
Cuhater <christian_krenn@hotmail.de>
2019-06-30 17:36:03 +0200

    Präsentation + Code fixes

--- MS3/Dienstnutzer/Dienstnutzer/routes/dashboard.js
+++ MS3/Dienstnutzer/Dienstnutzer/routes/dashboard.js
@@ -3,96 +3,54 @@ var router = express.Router();
 const functions = require('../public/javascripts/functions')
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-    /* console.log(req.body);
-     console.log(req.params)
-     console.log(req.query)*/
+router.get('/', async function (req, res) {
     let userID;
     userID = getSessionUserID();
 
-
     // CHECK IF QUERY EMPTY IF NOT CHECK IF SCORE EMTPY AND CHANGE IT
     if (req.query !== {}) {
+
         if (req.query.score !== undefined) {
-            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-            console.log("SCORE " + req.query.score);
-
-
             await updatePlayerScore(req.query.score);
-            let currentScore = await getPlayerScore(userID)
-
-            console.log("PLAYERSCORE" + currentScore + "\n\n\n")
-
+            let currentScore = await getPlayerScore(userID);
             updateSessionPlayerScore(currentScore);
 
-
-            console.log("\n\n" + currentScore + "\n\n wasgeht");
-            //res.render('dashboard', { title: 'Dashboard'});
         } else if (req.query.userID !== undefined) {
             let userIDreq = req.query;
             if (userID === undefined) {
                 console.log("SESSION USEr SET USER INCOMING" + userIDreq.userID);
-                let callback = await loginUser(userIDreq.userID)
+                let callback = await loginUser(userIDreq.userID);
 
                 if (callback === 0) {
                     res.render('userNotFound', {title: "user id not found"});
                 }
-
-                /*
-                console.log("test vor der rissmethode");
-                let myTest = await setUserID(userIDreq.userID);
-                console.log(myTest);
-                console.log("MYYYYYYYYYYYYYYYTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST" + JSON.stringify(myTest, null, 2));
-
-                let status = await postUsers(myTest);
-                console.log("hat geklappt?" + status)
-                let testscore = await getPlayerScore(userIDreq.userID);
-                console.log(testscore + " testi scori")
-
-                 */
             } else {
-                console.log("User is defined : " + userID);
-
+                console.log("User is already defined : " + userID);
             }
         }
-
         res.render('dashboard', {title: 'Dashboard'});
     }
-
-
 });
+
 router.post('/', async function (req, res) {
 
 
     let allUsers = await getUsers();
     let allUsersArray = allUsers['users'];
-
     let possibleID = allUsersArray.length;
 
 
     for (let i = 0; i < allUsersArray.length; i++) {
-
-        console.log("HALLO +" + i)
         if (parseInt(allUsersArray[i].id) === possibleID) {
-            console.log(" SHIED IS SCHON VORHANDEN +" + possibleID + " MEH " + allUsersArray[i])
             possibleID += 1;
             i = 0;
-        } else {
-            //if(parseInt(allUsersArray[i].id)   ===
-            console.log("test " + allUsersArray[i].id + "userID      " + possibleID)
         }
-
     }
     console.log(" POSSIBLE ID WÄRE : " + possibleID);
-
     let updatedUsers = await createNewUser(possibleID);
     await postUsers(updatedUsers);
-
-
     // TODO : SAVE new User in DB
 
-
-    console.log("\n ALARM \n")
     res.render('userCreated', {title: 'user Created', newID: possibleID});
 })
 
--- MS3/Dienstnutzer/Dienstnutzer/routes/quiz.js
+++ MS3/Dienstnutzer/Dienstnutzer/routes/quiz.js
@@ -16,62 +16,10 @@ router.get('/', async function (req, res) {
             console.log(myQuery)
         }
     }
-
-/*
-    if (req.query !== {}) {
-
-    }
-    if (req.query.type === 'species') {
-        console.log("SPECIES AUFGERUFEN")
-        myQuery = '?type=species'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'planets') {
-        console.log("PLANETS AUFGERUFEN")
-        myQuery = '?type=planets'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'people') {
-        console.log("PEOPLE AUFGERUFEN")
-        myQuery = '?type=people'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'vehicles') {
-        console.log("VEHICLES AUFGERUFEN")
-        myQuery = '?type=vehicles'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'starships') {
-        console.log("STARSHIPS AUFGERUFEN")
-        myQuery = '?type=starships'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'films') {
-        console.log("FILMS AUFGERUFEN")
-        myQuery = '?type=films'
-        console.log(myQuery)
-    }
-    if (req.query.type === 'custom') {
-        console.log("CUSTOM AUFGERUFEN")
-        myQuery = ''
-        console.log(myQuery)
-    }
-*/
-
-
-
-
-
-
-
-
-    let data = '';
-
-    let recievedQuestionArray = [];
     let count = 0;
     let btnTxt = "Play question 1/5"
     let questionArray;
-    let quizScore
+    let quizScore;
 
     if(req.query.score === undefined)
     {
@@ -81,34 +29,6 @@ router.get('/', async function (req, res) {
     {
         quizScore = req.query.score;
     }
-
-
-/*    if(req.query.score === '1'){
-        quizScore = 1;
-    }
-    else if(req.query.score === '2'){
-        quizScore = 2;
-    }
-    else if(req.query.score === '3'){
-        quizScore = 3;
-    }
-    else if(req.query.score === '4'){
-        quizScore = 4;
-    }
-    else if(req.query.score === '5'){
-        quizScore = 5;
-    }
-    else
-    {
-        console.error("kein score");
-        quizScore = 0;
-    }*/
-
-
-
-
-
-
     if (req.query !== {}) {
         questionArray = getLocalQuizArray();
     }
@@ -116,7 +36,6 @@ router.get('/', async function (req, res) {
     // TODO : CHECK TYPE - - ?q=0 .. quiz?q=1 ... usw
 
     if (req.query.question === '1') {
-
         count = 1;
         btnTxt = "Play question 2/5"
     } else if (req.query.question === '2') {
@@ -131,12 +50,8 @@ router.get('/', async function (req, res) {
     } else if (req.query.question === '5') {
 
         let finalQuizScore = quizScore;
-        console.log(" \n\n YOU GOT "+ finalQuizScore + " POINTS YOO \n\n")
-        // TODO : HEEEEEEEEEERE TOOOOODO GO ON 
         setTimeout(function () {
             res.redirect('/dashboard?score=' + finalQuizScore);
-
-            //alert("HIER WAS MACHEN ")
         }, 3000); // 5 seconds
     } else {
         questionArray = await getQuizData(myQuery);
@@ -144,14 +59,8 @@ router.get('/', async function (req, res) {
     }
     console.log("b4 AWAIT :O")
 
-
-    // TODO : GET QUIZ AS OBJECT
-
-
     let currentQuestion = questionArray[count]
-
     let questionText = currentQuestion[0];
-
     let answers = currentQuestion[1];
     let a1 = answers[0]
     let a2 = answers[1]
@@ -159,21 +68,6 @@ router.get('/', async function (req, res) {
     let a4 = answers[3]
     let ra = currentQuestion[2]
     let df = currentQuestion[3];
-
-    console.log("\n\n\n HAHAHAHA \n " +df)
-
-
-    //let innerKey = Object.keys(data)
-    /*    console.log(jsonData.length)
-        console.log(jsonData[0])
-        console.log(jsonData[1])
-        console.log(jsonData[2])
-
-        let anserws = jsonData[1];*/
-    /*            let a0 = anserws[0]
-                let a1 = anserws[1]
-                let a2 = anserws[2]
-                let a3 = anserws[3]*/
 
     if (req.query.question !== '5')
     {
@@ -192,38 +86,6 @@ router.get('/', async function (req, res) {
         });
 
     }
-
-
-
-    // TODO : Wenn nix, dann gib ihm GET auf Quiz
-
-    // TODO : in quiz todo save Data local und hoffe das die bleiben xD
-    /*   await http.get('http://localhost:3000/quiz', (resp) => {
-
-
-           // A chunk of data has been recieved.
-           resp.on('data', (chunk) => {
-               data += chunk;
-               console.log("I GOT DATA")
-           });
-
-           // The whole response has been received. Print out the result.
-           resp.on('end', () => {
-               //console.log(JSON.parse(data));
-               recievedQuestionArray = JSON.parse(data);
-
-               console.log("WACKEN IS VORBEI IHR RIPPER");
-
-
-
-           });
-
-       }).on("error", (err) => {
-           console.log("Error: " + err.message);
-       });
-
-   */
-
 });
 
 module.exports = router;
